75147007834ff9f9525be4e9de1b8050956b7a93
Xavier Contreras <xcontreras94@gmail.com>
2019-09-02 19:34:16 -0400

    Add login and HTML templating

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,30 @@
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
+
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': "Xavier"}
+    posts = [
+        {
+            'author': {'username': 'Brian'},
+            'body': 'Beautiful day in New York City!'
+        },
+        {
+            'author': {'username': 'Brenna'},
+            'body': 'Anybody else want to play outside?'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
