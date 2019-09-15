6774081910f75d0800a73712935099757b6de8c7
Xavier Contreras <xcontreras94@gmail.com>
2019-09-15 12:02:43 -0400

    Add last login and about me for users

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from wtforms import StringField, PasswordField, BooleanField, \
+    SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo, \
+    ValidationError, Length
 from app.models import User
 
 
@@ -28,3 +30,9 @@ class RegistrationForm(FlaskForm):
         email = User.query.filter_by(email=email.data).first()
         if email is not None:
             raise ValidationError('Please use a different email address.')
+
+
+class EditProfileFrom(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
